39aed25d86120fbcc08f391bca85fd9d666dea7b
shreya <89719271+ShreyaPSawant@users.noreply.github.com>
2024-03-19 22:33:02 +0530

    basic dashboard updated

--- Dashboard.py
+++ Dashboard.py
@@ -24,36 +24,256 @@ with st.container():
 
 col1, col2, col3 = st.columns(3)
 col1.metric("Monthly Sales", "20K", "-1.2%")
-col2.metric("Annual Sales", "50K", "8%")
-col3.metric("Predicted Sales", "86K", "4%")
+col2.metric("Weekly Sales", "50K", "8%")
+col3.metric("Weekly Predicted Sales", "86K", "4%")
 # #--------------------------------------
 
 overallSales_df = pd.read_csv("SalesData.csv")
 selected_columns = ['SalesPrice','SalesDate']  
 Sales1_df = overallSales_df[selected_columns].copy()
 
-calendar_options = {
-    "headerToolbar": {
-        "left": "today prev,next",
-        "center": "title",
-        "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
-    },
-    "slotMinTime": "06:00:00",
-    "slotMaxTime": "18:00:00",
-    "initialView": "resourceTimelineDay",
-    "resources": [
-        {"id": "a", "building": "Building A", "title": "Building A"},
-        # Add more resource entries as needed
-    ],
-}
+alert_list = ['jack daniel', 'old monk', 'amrut']
 
-calendar_events = [
-    {"title": "Event 1", "start": "2023-07-31T08:30:00", "end": "2023-07-31T10:30:00", "resourceId": "a"},
-    # Add more event entries as needed
+
+
+
+mode="daygrid"
+
+events = [
+    {
+        "title": "Event 1",
+        "color": "#FF6C6C",
+        "start": "2023-07-03",
+        "end": "2023-07-05",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 2",
+        "color": "#FFBD45",
+        "start": "2023-07-01",
+        "end": "2023-07-10",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 3",
+        "color": "#FF4B4B",
+        "start": "2023-07-20",
+        "end": "2023-07-20",
+        "resourceId": "c",
+    },
+    {
+        "title": "Event 4",
+        "color": "#FF6C6C",
+        "start": "2023-07-23",
+        "end": "2023-07-25",
+        "resourceId": "d",
+    },
+    {
+        "title": "Event 5",
+        "color": "#FFBD45",
+        "start": "2023-07-29",
+        "end": "2023-07-30",
+        "resourceId": "e",
+    },
+    {
+        "title": "Event 6",
+        "color": "#FF4B4B",
+        "start": "2023-07-28",
+        "end": "2023-07-20",
+        "resourceId": "f",
+    },
+    {
+        "title": "Event 7",
+        "color": "#FF4B4B",
+        "start": "2023-07-01T08:30:00",
+        "end": "2023-07-01T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 8",
+        "color": "#3D9DF3",
+        "start": "2023-07-01T07:30:00",
+        "end": "2023-07-01T10:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 9",
+        "color": "#3DD56D",
+        "start": "2023-07-02T10:40:00",
+        "end": "2023-07-02T12:30:00",
+        "resourceId": "c",
+    },
+    {
+        "title": "Event 10",
+        "color": "#FF4B4B",
+        "start": "2023-07-15T08:30:00",
+        "end": "2023-07-15T10:30:00",
+        "resourceId": "d",
+    },
+    {
+        "title": "Event 11",
+        "color": "#3DD56D",
+        "start": "2023-07-15T07:30:00",
+        "end": "2023-07-15T10:30:00",
+        "resourceId": "e",
+    },
+    {
+        "title": "Event 12",
+        "color": "#3D9DF3",
+        "start": "2023-07-21T10:40:00",
+        "end": "2023-07-21T12:30:00",
+        "resourceId": "f",
+    },
+    {
+        "title": "Event 13",
+        "color": "#FF4B4B",
+        "start": "2023-07-17T08:30:00",
+        "end": "2023-07-17T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 14",
+        "color": "#3D9DF3",
+        "start": "2023-07-17T09:30:00",
+        "end": "2023-07-17T11:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 15",
+        "color": "#3DD56D",
+        "start": "2023-07-17T10:30:00",
+        "end": "2023-07-17T12:30:00",
+        "resourceId": "c",
+    },
+    {
+        "title": "Event 16",
+        "color": "#FF6C6C",
+        "start": "2023-07-17T13:30:00",
+        "end": "2023-07-17T14:30:00",
+        "resourceId": "d",
+    },
+    {
+        "title": "Event 17",
+        "color": "#FFBD45",
+        "start": "2023-07-17T15:30:00",
+        "end": "2023-07-17T16:30:00",
+        "resourceId": "e",
+    },
+]
+calendar_resources = [
+    {"id": "a", "building": "Building A", "title": "Room A"},
+    {"id": "b", "building": "Building A", "title": "Room B"},
+    {"id": "c", "building": "Building B", "title": "Room C"},
+    {"id": "d", "building": "Building B", "title": "Room D"},
+    {"id": "e", "building": "Building C", "title": "Room E"},
+    {"id": "f", "building": "Building C", "title": "Room F"},
 ]
 
-custom_css = """
-    .fc-event-past {
+calendar_options = {
+    "editable": "true",
+    "navLinks": "true",
+    "resources": calendar_resources,
+    "selectable": "true",
+}
+
+if "resource" in mode:
+    if mode == "resource-daygrid":
+        calendar_options = {
+            **calendar_options,
+            "initialDate": "2023-07-01",
+            "initialView": "resourceDayGridDay",
+            "resourceGroupField": "building",
+        }
+    elif mode == "resource-timeline":
+        calendar_options = {
+            **calendar_options,
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "resourceTimelineDay",
+            "resourceGroupField": "building",
+        }
+    elif mode == "resource-timegrid":
+        calendar_options = {
+            **calendar_options,
+            "initialDate": "2023-07-01",
+            "initialView": "resourceTimeGridDay",
+            "resourceGroupField": "building",
+        }
+else:
+    if mode == "daygrid":
+        calendar_options = {
+            **calendar_options,
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "dayGridDay,dayGridWeek,dayGridMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "dayGridMonth",
+        }
+    elif mode == "timegrid":
+        calendar_options = {
+            **calendar_options,
+            "initialView": "timeGridWeek",
+        }
+    elif mode == "timeline":
+        calendar_options = {
+            **calendar_options,
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "timelineDay,timelineWeek,timelineMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "timelineMonth",
+        }
+    elif mode == "list":
+        calendar_options = {
+            **calendar_options,
+            "initialDate": "2023-07-01",
+            "initialView": "listMonth",
+        }
+    elif mode == "multimonth":
+        calendar_options = {
+            **calendar_options,
+            "initialView": "multiMonthYear",
+        }
+
+# state = calendar(
+#     # events=st.session_state.get("events", events),
+#     events=events,
+#     options=calendar_options,
+#     custom_css="""
+#     .fc-event-past {
+#         opacity: 0.8;
+#     }
+#     .fc-event-time {
+#         font-style: italic;
+#     }
+#     .fc-event-title {
+#         font-weight: 700;
+#     }
+#     .fc-toolbar-title {
+#         font-size: 2rem;
+#     }
+#     """,
+#     key=mode,
+# )
+
+    col1,col2=st.columns([2,1])
+    with col1:
+        # calendar = calendar(events=calendar_events, options=calendar_options, custom_css=custom_css)
+    # st.write(calendar)
+       st.write(calendar(
+    # events=st.session_state.get("events", events),
+    # events=events,
+       options=calendar_options,
+       custom_css="""
+      .fc-event-past {
         opacity: 0.8;
     }
     .fc-event-time {
@@ -65,15 +285,20 @@ custom_css = """
     .fc-toolbar-title {
         font-size: 2rem;
     }
-"""
-
-
-
-col1,col2=st.columns([2,1])
-with col1:
-    st.line_chart(Sales1_df, x="SalesDate", y="SalesPrice", color=["#FF0000"], width=100)
+    """#,
+  #  key=mode,
+      ))
 with col2:
-    calendar = calendar(events=calendar_events, options=calendar_options, custom_css=custom_css)
-    st.write(calendar)
+    st.subheader("Alerts!")
+    for i in alert_list:
+     st.markdown("- " + i)
+
+  
+
+
+st.line_chart(Sales1_df, x="SalesDate", y="SalesPrice", color=["#FF0000"], width=100)
+
+    
+        
 
 
